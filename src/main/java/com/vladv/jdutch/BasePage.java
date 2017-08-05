dc3ce90970ab9f56f372645b7aeda192d4096f45
atreides <vlad.vesa@gmail.com>
2017-08-05 13:41:24 +0300

    top menu will display actions in function of the current page

--- src/main/java/com/vladv/jdutch/BasePage.java
+++ src/main/java/com/vladv/jdutch/BasePage.java
@@ -1,28 +1,36 @@
 package com.vladv.jdutch;
 
 import java.io.Serializable;
+import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 
 import org.apache.wicket.ajax.AjaxRequestTarget;
+import org.apache.wicket.ajax.form.OnChangeAjaxBehavior;
 import org.apache.wicket.ajax.markup.html.form.AjaxButton;
 import org.apache.wicket.ajax.markup.html.form.AjaxSubmitLink;
 import org.apache.wicket.markup.html.WebPage;
 import org.apache.wicket.markup.html.basic.Label;
+import org.apache.wicket.markup.html.form.DropDownChoice;
 import org.apache.wicket.markup.html.form.Form;
 import org.apache.wicket.markup.html.list.ListItem;
 import org.apache.wicket.markup.html.list.ListView;
 import org.apache.wicket.model.LoadableDetachableModel;
+import org.apache.wicket.model.Model;
 import org.apache.wicket.request.mapper.parameter.PageParameters;
 
 public class BasePage extends WebPage {
-
 	private static final long serialVersionUID = 1L;
 
+	protected static final String TYPE_EN_DT = "English to Dutch";
+	protected static final String TYPE_DT_EN = "Dutch to English";
+	
+	protected String testtype = TYPE_EN_DT;
 	protected int lesson = 0;
-
 	protected int listItemPosition = 1;
+
+	protected boolean showtranslations = false;
 	
 	public BasePage(final PageParameters parameters) {
 		super(parameters);
@@ -30,6 +38,44 @@ public class BasePage extends WebPage {
 		Label header = new Label("lesson");
 		add(header);
 		header.setOutputMarkupId(true);
+		
+		Form<String> formlang = new Form<String>("formlang") {
+			@Override
+			public boolean isVisible() {
+				return showToolbar();
+			}
+		};
+		add(formlang);
+		
+		formlang.add(new AjaxButton("showall") {
+			private static final long serialVersionUID = 1L;
+			
+			@Override
+			protected void onSubmit(AjaxRequestTarget target) {
+				showtranslations = !showtranslations;
+				showTranslations(target);
+				if(showtranslations) {
+					this.setModel(getLoad("Hide Translations"));
+				} else {
+					this.setModel(getLoad("Translate"));
+				}
+				target.add(this);
+			}
+		});
+		
+		DropDownChoice<String> testChoice = new DropDownChoice("testtype", new Model(TYPE_EN_DT), Model.of(Arrays.asList(TYPE_EN_DT, TYPE_DT_EN)));
+		formlang.add(testChoice);
+		testChoice.setNullValid(false);
+		
+		testChoice.add(new OnChangeAjaxBehavior() {
+			private static final long serialVersionUID = 1L;
+
+			@Override
+			protected void onUpdate(AjaxRequestTarget target) {
+				testtype = testChoice.getDefaultModelObjectAsString();
+				testTypeChanged(target);
+			}
+		});
 
 		Form<Bean> lessonsForm = new Form<Bean>("lessonsForm");
 		add(lessonsForm);
@@ -63,10 +109,21 @@ public class BasePage extends WebPage {
 						target.add(header);
 					}
 				};
-				// TODO add button text here
 				item.add(ajaxSubmitLink);
 			}
 		});
+	}
+
+	protected boolean showToolbar() {
+		return false;
+	}
+
+	protected void showTranslations(AjaxRequestTarget target) {
+		
+	}
+
+	protected void testTypeChanged(AjaxRequestTarget target) {
+		
 	}
 
 	protected void lessonChanged(AjaxRequestTarget target) {
@@ -98,5 +155,16 @@ public class BasePage extends WebPage {
 		public void setText(String text) {
 			this.text = text;
 		}
+	}
+	
+	protected boolean isSwitched() {
+		return testtype.equals(TYPE_EN_DT);
+	}
+	
+	protected String getFrom() {
+		return testtype.equals(TYPE_EN_DT) ? "English" : "Dutch";
+	}
+	protected String getTo() {
+		return testtype.equals(TYPE_EN_DT) ? "Dutch" : "English";
 	}
 }
