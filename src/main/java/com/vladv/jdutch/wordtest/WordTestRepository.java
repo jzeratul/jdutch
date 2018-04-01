63a72290557e06e3656d036abdccc067e70a1746
Vlad Vesa <vlad.vesa@gmail.com>
2018-04-01 13:19:09 +0300

    Wrong jpa pattern for custom method generation

--- src/main/java/com/vladv/jdutch/wordtest/WordTestRepository.java
+++ src/main/java/com/vladv/jdutch/wordtest/WordTestRepository.java
@@ -1,12 +1,8 @@
 package com.vladv.jdutch.wordtest;
 
-import java.util.List;
-
 import org.springframework.data.mongodb.repository.MongoRepository;
 
 public interface WordTestRepository extends MongoRepository<WordTest, Long> {
 
 	void deleteWordTestByTestname(String testname);
-
-  List<WordTest> findAllOrderByTestNameSortAsc();
 }
