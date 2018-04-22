feed494cdd404361d34980b7119edddb8bf79a2d
Vlad Vesa <vlad.vesa@gmail.com>
2018-04-22 23:34:41 +0300

    Added error feedback for gaatentest

--- src/main/resources/static/js/gaatentestpage.js
+++ src/main/resources/static/js/gaatentestpage.js
@@ -1,3 +1,4 @@
+
 replaceAllBoldElementsWithInput = function() {
 	
 	var idx = 0;
@@ -7,8 +8,9 @@ replaceAllBoldElementsWithInput = function() {
 		el.replaceWith(function() {
 			
 			var txt = $(this).contents().text();
-			var newelem = "<input type='text' autocomplete='off' name='newvalue"+idx+"' id='newvalue"+idx+"' class='newvalues' />";
-			var hidelem = "<input type='text' autocomplete='off' name='initialvalue"+idx+"' id='initialvalue"+idx+"' value='"+txt+"' class='oldvalues' hidden='hidden'  />";
+			var newelem = "<input type='text' autocomplete='off' name='newvalue"+idx+"' id='newvalue"+idx+"' class='newvalues' />" +
+					"<span class='btn btn-danger hideme' id='result"+idx+"'></span>";
+			var hidelem = "<input type='text' autocomplete='off' name='initialvalue"+idx+"' id='initialvalue"+idx+"' value='"+txt+"' class='oldvalues' hidden='hidden'/>";
 			idx++;
 			return newelem = newelem.concat(hidelem);
 	    });
@@ -17,4 +19,18 @@ replaceAllBoldElementsWithInput = function() {
 
 submitForm = function() {
 
+	var inputs = $('.newvalues');
+	var x = 0;
+	for(var i in inputs) {
+		
+		var v1 = $('#newvalue' + x).val();
+		var v2 = $('#initialvalue' + x).val();
+		
+		if(v1 == v2) {
+		} else {
+			$('#result' + x).removeClass("hideme");
+			$('#result' + x).html($('#initialvalue' + x).val());
+		}
+		x++;
+	}
 }
